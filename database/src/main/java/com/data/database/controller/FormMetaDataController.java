67663c704b865563db472a0d62cf3bf9f41ff06d
Kingsuk Biswas <kingsuk2k@gmail.com>
2023-07-31 22:52:42 +0530

    testing

--- database/src/main/java/com/data/database/controller/FormMetaDataController.java
+++ database/src/main/java/com/data/database/controller/FormMetaDataController.java
@@ -2,14 +2,11 @@ package com.data.database.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.data.database.dto.FormDataRequest;
 import com.data.database.dto.FormMetaDataRequest;
 import com.data.database.service.FormDataService;
 
@@ -18,39 +15,16 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping(value = "/db")
 @Slf4j
-public class FormController {
+public class FormMetaDataController {
 
     @Autowired
     private FormDataService fService;
 
-    @PostMapping("/form/save")
+    @PostMapping("/form/meta/save")
     public ResponseEntity<?> storeformDetails(@RequestBody FormMetaDataRequest fDataRequest) {
         try {
             fService.saveFormMetaData(fDataRequest);
             return ResponseEntity.ok().build();
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return ResponseEntity.badRequest().build();
-
-        }
-    }
-
-    @PostMapping("/data/save")
-    public ResponseEntity<?> storeFormData(@RequestBody FormDataRequest fDataRequest) {
-        try {
-            fService.saveAnswer(fDataRequest);
-            return ResponseEntity.ok().build();
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return ResponseEntity.badRequest().build();
-
-        }
-    }
-
-    @GetMapping("/data/all")
-    public ResponseEntity<?> storeFormData(@RequestHeader String name) {
-        try {
-            return ResponseEntity.ok().body(fService.getAllAnswersByForm(name));
         } catch (Exception e) {
             log.error(e.getMessage());
             return ResponseEntity.badRequest().build();
